32a3e2f3552ff027e00aee5c3faf9ec5cbdab059
FatihThaDev <fatih.mekic@stu.ibu.edu.ba>
2025-10-03 16:30:33 +0200

    Added courses.html page with simple category filter.

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,9 +1,8 @@
-'use strict';
-
 (function() {
   function init() {
     var router = new Router([
       new Route('home', 'home.html', true),
+      new Route('courses', 'courses.html'),
       new Route('login', 'login.html'),
       new Route('register', 'register.html'),
     ]);
